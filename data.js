47a728382202627d88bbc10fdd364862ca236c9a
wxzwsj197950 <935817127@qq.com>
2015-12-23 16:15:40 +0800

    have fun

---
+++ data.js
@@ -0,0 +1,36 @@
+var v={
+  "songs": [
+    {
+      "name": "Talking Body",
+      "url": "http://m2.music.126.net/W3bNMzQy2TJpN9WZkSnChA==/2539871860597335.mp3",
+      "artists": [
+        "Tove Lo"
+      ],
+      "picUrl": "http://p3.music.126.net/XZ_nsjD886M30JW2uJu4CQ==/6659741930003999.jpg"
+    },
+    {
+      "name": "Can't Feel My Face",
+      "url": "http://m2.music.126.net/3hgLxYkHD6jXf19ptDq8sg==/2933497025034124.mp3",
+      "artists": [
+        "The Weeknd"
+      ],
+      "picUrl": "http://p3.music.126.net/1ODgbpkXF6O5KVholm2FZw==/7952767603909010.jpg"
+    },
+    {
+      "name": "Stitches",
+      "url": "http://m2.music.126.net/6i6fRe_jfS3NI64c8GO2HA==/7909886650751437.mp3",
+      "artists": [
+        "Shawn Mendes"
+      ],
+      "picUrl": "http://p4.music.126.net/9hc8bW27GiexAulNeyJ1UQ==/7699879929620183.jpg"
+    },
+    {
+      "name": "Counting Stars",
+      "url": "http://m2.music.126.net/74xfWneCZOVArt77MESkew==/7956066140923388.mp3",
+      "artists": [
+        "OneRepublic"
+      ],
+      "picUrl": "http://p3.music.126.net/b2nl6jsVbqj23IV8dVvJcg==/7766950139663735.jpg"
+    }
+  ]
+}
